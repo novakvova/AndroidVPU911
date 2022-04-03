98660f59bcf2757a832c340bd3dd2c6f6ab66792
novakvova <novakvova@gmail.com>
2022-04-03 13:23:22 +0300

    Add register upload image

--- Android/app/src/main/java/com/example/atb/MainActivity.java
+++ Android/app/src/main/java/com/example/atb/MainActivity.java
@@ -8,11 +8,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.atb.account.RegisterActivity;
 import com.example.atb.network.account.AccountService;
 import com.example.atb.network.account.dto.AccountResponseDTO;
 import com.example.atb.network.account.dto.RegisterDTO;
@@ -31,156 +35,39 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvInfo;
-    private TextInputLayout textFieldEmail;
-    private TextInputEditText txtEmail;
 
-    private TextInputLayout textFieldFirstName;
-    private TextInputEditText txtFirstName;
-
-    // One Preview Image
-    ImageView IVPreviewImage;
-    // constant to compare
-    // the activity result code
-    int SELECT_PICTURE = 200;
-    String sImage="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvInfo = findViewById(R.id.tvInfo);
-        textFieldEmail = findViewById(R.id.textFieldEmail);
-        txtEmail = findViewById(R.id.txtEmail);
-
-        textFieldFirstName = findViewById(R.id.textFieldFirstName);
-        txtFirstName = findViewById(R.id.txtFirstName);
-
-        IVPreviewImage = findViewById(R.id.IVPreviewImage);
-
     }
 
-    public void handleSelectImageClick(View view) {
-        // create an instance of the
-        // intent of the type image
-        Intent i = new Intent();
-        i.setType("image/*");
-        i.setAction(Intent.ACTION_GET_CONTENT);
 
-        // pass the constant to compare it
-        // with the returned requestCode
-        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
-    }
-
-    // this function is triggered when user
-    // selects the image from the imageChooser
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK) {
-
-            // compare the resultCode with the
-            // SELECT_PICTURE constant
-            if (requestCode == SELECT_PICTURE) {
-                // Get the url of the image from data
-                Uri uri = data.getData();
-                // update the preview image in the layout
-                IVPreviewImage.setImageURI(uri);
-                Bitmap bitmap= null;
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // initialize byte stream
-                ByteArrayOutputStream stream=new ByteArrayOutputStream();
-                // compress Bitmap
-                bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
-                // Initialize byte array
-                byte[] bytes=stream.toByteArray();
-                // get base64 encoded string
-                sImage= Base64.encodeToString(bytes,Base64.DEFAULT);
-            }
-        }
-    }
-
-    public void handleClick(View view) {
-        RegisterDTO registerDTO = new RegisterDTO();
-        registerDTO.setEmail(txtEmail.getText().toString());
-        registerDTO.setFirstName(txtFirstName.getText().toString());
-        registerDTO.setSecondName("Ivanov");
-        registerDTO.setPhone("99382833");
-        registerDTO.setPassword("12345");
-        registerDTO.setConfirmPassword("12345");
-        registerDTO.setPhoto(sImage);
-
-        if (!validationFields(registerDTO))
-            return;
-
-        AccountService.getInstance()
-                .jsonApi()
-                .register(registerDTO)
-                .enqueue(new Callback<AccountResponseDTO>() {
-                    @Override
-                    public void onResponse(Call<AccountResponseDTO> call, Response<AccountResponseDTO> response) {
-                        if (response.isSuccessful()) {
-                            AccountResponseDTO data = response.body();
-                            tvInfo.setText("response is good");
-                        } else {
-                            try {
-                                showErrorsServer(response.errorBody().string());
-                            } catch (Exception e) {
-                                System.out.println("------Error response parse body-----");
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<AccountResponseDTO> call, Throwable t) {
-                        String str = t.toString();
-                        int a = 12;
-                    }
-                });
-    }
-
-    private boolean validationFields(RegisterDTO registerDTO) {
-        textFieldEmail.setError("");
-        if (registerDTO.getEmail().equals("")) {
-            textFieldEmail.setError("Вкажіть пошту");
-            return false;
-        }
-
-        textFieldFirstName.setError("");
-        if (registerDTO.getFirstName().equals("")) {
-            textFieldFirstName.setError("Вкажіть ім'я");
-            return false;
-        }
-
-        textFieldFirstName.setError("");
-        if (sImage.equals("")) {
-            textFieldFirstName.setError("Обнріть фотку");
-            return false;
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
         return true;
     }
 
-    private void showErrorsServer(String json) {
-        Gson gson = new Gson();
-        ValidationRegisterDTO result = gson.fromJson(json, ValidationRegisterDTO.class);
-        String str = "";
-        if (result.getErrors().getEmail() != null) {
-            for (String item : result.getErrors().getEmail()) {
-                str += item + "\n";
-            }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.m_register:
+                try {
+                    intent = new Intent(MainActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                }
+                catch(Exception ex) {
+                    System.out.println("Problem "+ ex.getMessage());
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        textFieldEmail.setError(str);
 
-        str = "";
-        if (result.getErrors().getFirstName() != null) {
-            for (String item : result.getErrors().getFirstName()) {
-                str += item + "\n";
-            }
-        }
-        textFieldFirstName.setError(str);
     }
+
 }
---
+++ Android/app/src/main/java/com/example/atb/account/RegisterActivity.java
@@ -0,0 +1,187 @@
+package com.example.atb.account;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.atb.MainActivity;
+import com.example.atb.R;
+import com.example.atb.network.account.AccountService;
+import com.example.atb.network.account.dto.AccountResponseDTO;
+import com.example.atb.network.account.dto.RegisterDTO;
+import com.example.atb.network.account.dto.ValidationRegisterDTO;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.gson.Gson;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextView tvInfo;
+    private TextInputLayout textFieldEmail;
+    private TextInputEditText txtEmail;
+
+    private TextInputLayout textFieldFirstName;
+    private TextInputEditText txtFirstName;
+
+    // One Preview Image
+    ImageView IVPreviewImage;
+    // constant to compare
+    // the activity result code
+    int SELECT_PICTURE = 200;
+    String sImage="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        tvInfo = findViewById(R.id.tvInfo);
+        textFieldEmail = findViewById(R.id.textFieldEmail);
+        txtEmail = findViewById(R.id.txtEmail);
+
+        textFieldFirstName = findViewById(R.id.textFieldFirstName);
+        txtFirstName = findViewById(R.id.txtFirstName);
+
+        IVPreviewImage = findViewById(R.id.IVPreviewImage);
+
+    }
+
+    public void handleSelectImageClick(View view) {
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri uri = data.getData();
+                // update the preview image in the layout
+                IVPreviewImage.setImageURI(uri);
+                Bitmap bitmap= null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // initialize byte stream
+                ByteArrayOutputStream stream=new ByteArrayOutputStream();
+                // compress Bitmap
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+                // Initialize byte array
+                byte[] bytes=stream.toByteArray();
+                // get base64 encoded string
+                sImage= Base64.encodeToString(bytes,Base64.DEFAULT);
+            }
+        }
+    }
+
+    public void handleClick(View view) {
+        RegisterDTO registerDTO = new RegisterDTO();
+        registerDTO.setEmail(txtEmail.getText().toString());
+        registerDTO.setFirstName(txtFirstName.getText().toString());
+        registerDTO.setSecondName("Ivanov");
+        registerDTO.setPhone("99382833");
+        registerDTO.setPassword("12345");
+        registerDTO.setConfirmPassword("12345");
+        registerDTO.setPhoto(sImage);
+
+        if (!validationFields(registerDTO))
+            return;
+        AccountService.getInstance()
+                .jsonApi()
+                .register(registerDTO)
+                .enqueue(new Callback<AccountResponseDTO>() {
+                    @Override
+                    public void onResponse(Call<AccountResponseDTO> call, Response<AccountResponseDTO> response) {
+                        if (response.isSuccessful()) {
+                            AccountResponseDTO data = response.body();
+                            //tvInfo.setText("response is good");
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            try {
+                                showErrorsServer(response.errorBody().string());
+                            } catch (Exception e) {
+                                System.out.println("------Error response parse body-----");
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<AccountResponseDTO> call, Throwable t) {
+                        String str = t.toString();
+                        int a = 12;
+                    }
+                });
+    }
+
+    private boolean validationFields(RegisterDTO registerDTO) {
+        textFieldEmail.setError("");
+        if (registerDTO.getEmail().equals("")) {
+            textFieldEmail.setError("Вкажіть пошту");
+            return false;
+        }
+
+        textFieldFirstName.setError("");
+        if (registerDTO.getFirstName().equals("")) {
+            textFieldFirstName.setError("Вкажіть ім'я");
+            return false;
+        }
+
+        textFieldFirstName.setError("");
+        if (sImage.equals("")) {
+            textFieldFirstName.setError("Обнріть фотку");
+            return false;
+        }
+        return true;
+    }
+
+    private void showErrorsServer(String json) {
+        Gson gson = new Gson();
+        ValidationRegisterDTO result = gson.fromJson(json, ValidationRegisterDTO.class);
+        String str = "";
+        if (result.getErrors().getEmail() != null) {
+            for (String item : result.getErrors().getEmail()) {
+                str += item + "\n";
+            }
+        }
+        textFieldEmail.setError(str);
+
+        str = "";
+        if (result.getErrors().getFirstName() != null) {
+            for (String item : result.getErrors().getFirstName()) {
+                str += item + "\n";
+            }
+        }
+        textFieldFirstName.setError(str);
+    }
+}
